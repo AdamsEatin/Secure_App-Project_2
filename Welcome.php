0322bc1a7e35c4dc42f587cea892a878a8e5ba04
AdamsEatin <adameaton76@gmail.com>
2018-03-04 07:48:01 +0000

    Added Welcome page, updated redirection to Welcome page

---
+++ Welcome.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+		<meta http-equiv="Pragma" content="no-cache" />
+		<meta http-equiv="Expires" content="0" />
+		<title>Secure App 2.0 : Welcome </title>
+		<link rel="stylesheet" type="text/css" href="main_page.css">
+	</head>
+	
+	<body>
+		<h1>Welcome to the Secure App System 2.0!</h1>
+		
+		<h2>You've Successfully Logged In!</h2>
+		<h3>The standard Lorem Ipsum passage, used since the 1500s</h3>
+		<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."<br><br>
+		Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."<br>
+		Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."<br><br>
+		Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+		
+		<form>
+			<button formaction="/Index.php">Logout</button>
+		</form>
+	</body>
+</html>
